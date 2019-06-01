c6bbed1c0446185af1284edafc72b147bf60f0b1
Michael <mr201190@gmail.com>
2019-06-01 18:09:45 +0200

    use alias file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,3 @@
-#Alias
-alias fishconfig="nvim ~/.config/fish/config.fish"
-alias vimrc="nvim ~/.vimrc"
-alias i3config="nvim ~/.config/i3/config"
-alias xresource="nvim ~/.Xresources"
-alias xinitrc="nvim ~/.xinitrc"
-
-alias fm="~/.config/vifm/scripts/vifmrun"
-alias vifmimg="~/.config/vifm/scripts/vifmimg"
-alias vim="nvim"
-alias ..="cd .."
-alias dev="cd ~/development"
-alias music="cd /media/200gb/Eigene\ Musik"
-
-alias reboot="sudo systemctl reboot"
-alias poweroff="sudo systemctl poweroff"
-alias mountusb="sudo ~/scripts/mount.sh"
-alias r="ranger"
-alias youtube-split "~/development/youtube-split/youtube-split.sh"
-alias config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
-
-
+source ~/.config/aliasrc
 #Rust config, Rails config
-set PATH $HOME/.cargo/bin $HOME/.gem/ruby/2.5.0/bin $PATH
+set PATH $HOME/.gem/ruby/2.5.0/bin $PATH
